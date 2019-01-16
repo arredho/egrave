8ee0a6a6df826e804023caf6cdda285bc4dbed96
Ahmad Ridhaudin <redhoz_ganu@yahoo.com>
2019-01-17 04:52:07 +0800

    Initial commit

---
+++ resources/views/partials/footer.blade.php
@@ -0,0 +1,7 @@
+<footer>
+    <div class="footer-content container">
+        <div class="made-with">© Copyright 2019 Design by Ahmad Ridhaudin Bin Ismail</div>
+        <div class="made-with">© Copyright 2019 Design for Nor Aimuni Binti Md Rashid</div>
+        {{ menu('footer', 'partials.menus.footer') }}
+    </div> <!-- end footer-content -->
+</footer>
